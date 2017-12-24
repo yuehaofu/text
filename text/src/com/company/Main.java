023e047fd8cdcb50793a5f82c45f38b315a2326a
FUYUEHAO <13588014652@163.com>
2017-12-24 19:06:32 +0800

    test

---
+++ text/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	      int a=5;
+	      int b=7;
+	      int c = a+b;
+	      System.out.println(c);
+
+    }
+}
